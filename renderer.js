0df1776f533c4e66d2a2f7c0f1f7fb8faed0c179
Sumit Chohan <chauhansumit@gmail.com>
2018-10-11 22:15:39 -0500

    wip

--- renderer.js
+++ renderer.js
@@ -3,61 +3,91 @@
 // All of the Node.js APIs are available in this process.
 var ById = function (id) {
     return document.getElementById(id);
- }
- var jsonfile = require('jsonfile');
- var favicon = require('favicon-getter').default;
- var path = require('path');
- var uuid = require('uuid');
- var steps = path.join(__dirname, 'steps.json');
- 
- var view = ById('view'), views = ById('views'), login = ById('logIn');
- function login_click() {
-    view.setAttribute('src', 'https://login.microsoftonline.com/common/oauth2/authorize?response_type=id_token&client_id=5b6e0ea6-1a26-4b99-9dbf-11b7a6f996b4&redirect_uri=https%3A%2F%2Fsam.ttx.com%2Fadmin%2Fui%2Fapplications&domain_hint=ttx.com');
+}
+var jsonfile = require('jsonfile');
+var favicon = require('favicon-getter').default;
+var path = require('path');
+var uuid = require('uuid');
+var steps = path.join(__dirname, 'steps.json');
+
+var view = ById('view'), views = ById('views'), login = ById('logIn');
+function login_click() {
+    view.setAttribute('src', config[sam_env].url);
     console.log('login clicked');
-    views.hidden = false;
- }
- var token;
- var tokenKey=
- function finishedLoad(event) { 
-    if (view.src.toString().toLowerCase().indexOf('fs.ttx.com') > 0) {
-        var passwordInput=view.getWebContents().executeJavaScript('document.getElementById("passwordInput").id')
-        if(passwordInput)
+    acquireAccessToken(function(){console.log("token" + sam_access_token)});
+}
+var sam_env = 'tst'
+var config = {
+    qa: { url: 'https://sams.ttx.com/admin/ui/applications', clientId: 'f40733a5-34b3-4591-a174-025326a3fc03' },
+    prod: { url: 'https://sam.ttx.com/admin/ui/applications', clientId: 'f725f14f-c21f-4718-af77-cf85b2fd3bfd' },
+    dev: { url: 'https://samd.ttx.com/admin/ui/applications', clientId: '1ceb3870-a843-454c-aa8a-c4c2841aec51' },
+    tst: { url: 'https://samt.ttx.com/admin/ui/applications', clientId: '6871a66a-7346-455b-b879-2b676f080b6d' }
+}
+var sam_access_token;
+function setAccessToken(t)
+{
+    console.log(t);
+    sam_access_token=t;
+}
+function acquireAccessToken(callback)
+{
+    console.log("checking..")
+    if(sam_access_token)
+    {
+        callback();
+    }
+    else
+    {
+        setTimeout(() => {   acquireAccessToken(callback)         
+        }, 1000);
+    }
+} 
+var lastLoadedTime=new Date();
+function finishedLoad(event) {
+    lastLoadedTime=new Date();
+    setTimeout(function(){
+        if((new Date()-lastLoadedTime)>1998)
         {
-            passwordInput.then(function (s) {
-                if (s === "passwordInput") {
-                    view.style.height='480px';
+            if (view.src.toString().toLowerCase().indexOf('.ttx.com/admin') > 0 || view.src.toString().toLowerCase().indexOf('google') > 0) {
+                view.style.height = '0px';
+                if (view.src.toString().toLowerCase().indexOf('.ttx.com/admin') > 0) {
+                    tokenAccessKey='adal.access.token.key' + config[sam_env].clientId ;
+                    console.log(tokenAccessKey);
+                    var tokenResponse = view.getWebContents().executeJavaScript('localStorage["'+tokenAccessKey+ '"]');
+                    if (tokenResponse) {
+                        tokenResponse.then(function (s) {
+                            setAccessToken(s);
+                        });
+                    }
                 }
-            });
+            }
+            else
+            {
+                view.style.height = '480px';
+            }
         }
-    }
-    else {
-        view.style.height='0px';
-    }
+    },2000);
     console.log("Navigated to - " + view.src);
- }
- function acquireToken(env,success)
- {
-     
- }
- console.log(steps); 
- view.style.height='0px'
- login.addEventListener('click', login_click);
- view.addEventListener('did-finish-load', finishedLoad);
+} 
+console.log(sam_access_token);
+console.log(steps);
+view.style.height = '0px';
+login.addEventListener('click', login_click);
+view.addEventListener('did-finish-load', finishedLoad);
  // https://github.com/hokein/electron-sample-apps/blob/master/webview/browser/browser.js#L5
  // To Do add dev tools open ✔️
  // update url ✔️
  // add bookmark by pressing button ✔️
  // load all bookmarks when list is clicked ✔️
- 
- 
- 
+
+
+
  // To Do / Continue
  // Feedback when loading
  // Feedback with favorite icon to show that bookmark is not-added/added/already-added
  // Tabs !:@
  // Option to remove bookmarks.
- 
- 
- 
- 
- 
+
+
+
+
